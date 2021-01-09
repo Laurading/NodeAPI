ab9975ac7a0be2dd0ef8b17ac9844273a3d2e775
lyndaABDOUS <57401886+lyndaABDOUS@users.noreply.github.com>
2021-01-09 21:07:50 +0100

    Update AuthentificationRoute.ts

--- TestApi/nodeAPI/src/routes/AuthentificationRoute.ts
+++ TestApi/nodeAPI/src/routes/AuthentificationRoute.ts
@@ -1,16 +1,24 @@
 import { Router } from 'express';
-// import { Request, Response } from 'express';
-// import { AuthController } from '../controller/AuthController';
-// import { registerMidd, loginMidd, authMidd } from '../middlewares/auth.middleware';
+import { Request, Response } from 'express';
+import { AuthController } from '../controller/AuthController';
+import { subMidd, LoggedIdMidd } from '../middlewares/sub.middleware';
+import {  loginMidd, registerMidd } from '../middlewares/auth.middleware';
+import { SubscriptionController } from '../controller/SubscriptionController';
+import {tokenvalidation} from '../utils/verifyToken';
+
+
 
 
 
 const route: Router = Router();
 
-// route.get('/', authMidd, (req: Request, res: Response) => {
-//     return res.end('<h1>OOUUUIII tu es connecté</h1>')
-// })
-// route.post('/login', loginMidd, AuthController.login)
-// route.post('/register', registerMidd, AuthController.register)
+route.get('/',subMidd, (req: Request, res: Response) => {
+    return res.end('<p> hello word </p>')
+})
+
+route.post('/login',loginMidd, AuthController.login)
+route.post('/register', registerMidd, AuthController.register)
+route.put('/subscription',tokenvalidation ,  SubscriptionController.subscribe )
+
 
 export { route as AuthentificationRoute }
